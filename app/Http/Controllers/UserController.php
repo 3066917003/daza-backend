f7e79163a0753ba832e55c160317d40f30e64d0b
Lijy91 <lijy91@foxmail.com>
2016-05-03 23:28:31 +0800

    登录注册

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 
+use Auth;
+use Validator;
+
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -14,11 +17,42 @@ class UserController extends Controller
     public function __construct()
     {
         // 执行 auth 认证
-        $this->middleware('auth', ['except' => ['register', 'login', 'passwordReset']]);
+        $this->middleware('auth', [
+            'except' => [
+                'register',
+                'login',
+                'logout',
+                'passwordReset',
+                'show'
+            ]
+        ]);
     }
 
     public function register(Request $request)
     {
+        $username = $request->input('username');
+        $password = $request->input('password');
+
+        $params = array(
+            'username' => $username,
+            'password' => $password,
+        );
+
+        $rules = array(
+            'username' => 'required|max:255|unique:users',
+            'password' => 'required|min:6',
+        );
+        $validator = Validator::make($params, $rules);
+        if ($validator->fails()) {
+            return $this->failure($validator->errors()->all());
+        }
+
+        $params['password'] = bcrypt($password);
+
+        $user = User::create($params);
+        if ($user) {
+            return $this->login($request);
+        }
         return $this->failure();
     }
 
@@ -26,11 +60,26 @@ class UserController extends Controller
     {
         $username = $request->input('username');
         $password = $request->input('password');
+        $remember = true;
 
         $credentials = array(
-          'username' => $username,
-          'password' => $password
+            'username' => $username,
+            'password' => $password
         );
+
+        $rules = array(
+            'username' => 'required|max:255',
+            'password' => 'required|min:6',
+        );
+
+        $validator = Validator::make($credentials, $rules);
+        if ($validator->fails()) {
+            return $this->failure($validator->errors()->all());
+        }
+
+        if (Auth::attempt($credentials, $remember)) {
+            return $this->success(Auth::user());
+        }
 
         return $this->failure();
     }
@@ -63,7 +112,13 @@ class UserController extends Controller
 
     public function show(Request $request, $user_id)
     {
-        $result = User::find($user_id);
+        $rules = array('user' => 'exists:users,id');
+        $validator = Validator::make(['user' => $user_id], $rules);
+        if ($validator->fails()) {
+            return $this->failure($validator->errors()->all());
+        }
+        $user = User::find($user_id);
+        return $this->success($user);
     }
 
     public function update(Request $request)
