b27697ebb0536960fdb38d5b51968481d873b2d5
Lijy91 <lijy91@foxmail.com>
2016-05-27 18:11:33 +0800

    test

--- app/Console/Commands/Inspire.php
+++ app/Console/Commands/Inspire.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Models\Tweet;
+use DB;
 use Illuminate\Console\Command;
 use Illuminate\Foundation\Inspiring;
 
@@ -29,9 +30,8 @@ class Inspire extends Command
      */
     public function handle()
     {
-        Tweet::create(
+        DB::table('tweets')->insert(
             ['content' => 'Hi']
         );
-        $this->comment(PHP_EOL.Inspiring::quote().PHP_EOL);
     }
 }
